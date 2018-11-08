5c487723aba078c4c12ff086eed1083b127737fa
sergioprofe <44881403+sergioprofe@users.noreply.github.com>
2018-11-08 23:14:00 +0100

    Create prova1.php

---
+++ prova1.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="Sergio" content="5Ci pagina php">
+<title>Costruire un sito in PHP</title>
+
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
+<style>@import url("CSS/style.css");</style>
+</head>
+
+<body>
+<header>
+	<img src="5C.jpg" alt="">
+	<h1>5Ci informatica</h1>
+    <h2>ITIS G. Marconi</h2>
+</header>
+
+
+<nav>
+    <a href="#">Home</a>
+</nav>
+
+<article>
+	<div class="content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>Ut enim ad minim veniam, quis nostrud exercitation ullamco<br> laboris nisi ut aliquip ex ea commodo consequat.</p> 
+    </div>
+</article>
+
+<section>
+	<div class="content clearfix">
+        <div class="section">
+        	<h3>Corso TPI</h3>
+        </div>
+        <div class="section red">
+        	<h3>Corso Sistemi</h3>
+        </div>
+        <div class="section yellow">
+        	<h3>Corso PHP</h3>
+        </div>
+    </div>
+</section>
+
+<footer>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+</footer>
+</body>
+</html>
